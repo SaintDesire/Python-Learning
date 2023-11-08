772dff84764bce51f617b803f699817ed09d7a40
Nikita Korshun <nikitakorshun@mail.ru>
2023-11-08 23:54:26 +0300

    fixed weather app

--- webApps/weather/views.py
+++ webApps/weather/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Subquery
 from django.shortcuts import render, redirect
 import requests
 from weather.forms import CityForm
@@ -16,25 +17,41 @@ def weather(request):
         form = CityForm(request.POST)
         if form.is_valid():
             city_name = form.cleaned_data["name"]
-            if City.objects.filter(name=city_name).exists() == False:
-                form.save()
-                return redirect('weather')
+            res = requests.get(url.format(city_name)).json()
+            if res["cod"] == 200:
+                if City.objects.filter(name=city_name).exists() == False:
+                    form.save()
+                    return redirect('weather')
+                else:
+                    error = 'City is already on the list'
             else:
-                error = 'City is already on the list'
+                error = "City not found in the external API"
         else:
             error = 'Error'
 
+    total_cities = City.objects.count()
+    if total_cities > 10:
+        records_to_delete = total_cities - 10
+        ids_to_delete = City.objects.order_by('id')[:records_to_delete].values('id')
+        City.objects.filter(id__in=Subquery(ids_to_delete)).delete()
+
     for city in cities:
         res = requests.get(url.format(city.name)).json()
-        if int(res["main"]["temp"]) > 0:
-            temp = '+' + str(int(res["main"]["temp"]))
-        city_info = {
-            'city': city.name,
-            'temp': temp,
-            'icon': res["weather"][0]["icon"],
-            'id' : city.id
-        }
-        all_cities.append(city_info)
+
+        if res["cod"] == 200:
+            if City.objects.filter(name=city.name).exists() == False:
+                error = "City not found"
+            if int(res["main"]["temp"]) > 0:
+                temp = '+' + str(int(res["main"]["temp"]))
+                city_info = {
+                    'city': city.name,
+                    'temp': temp,
+                    'icon': res["weather"][0]["icon"],
+                    'id': city.id
+                }
+                all_cities.append(city_info)
+
+
 
     data = {
         'all_info': all_cities,
