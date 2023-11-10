15af0388852ac1f5778382d00ee77de6b7fae749
Nikita Korshun <nikitakorshun@mail.ru>
2023-11-10 19:39:51 +0300

    add currency converter app

--- webApps/main/urls.py
+++ webApps/main/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
     path('todo',include('todo.urls')),
     path('weather', include('weather.urls')),
+    path('converter', include('currency_converter.urls')),
 ]
--- webApps/webApps/settings.py
+++ webApps/webApps/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'todo',
-    'weather'
+    'weather',
+    'currency_converter'
 ]
 
 MIDDLEWARE = [
